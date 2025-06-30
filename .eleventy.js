b5f037d25fefb92ac1cf2c511865d28cccefc766
TMJC-BR <brendans.smallhome@gmail.com>
2025-06-30 08:38:30 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("_site/style.css");
+        eleventyConfig.addPassthroughCopy("_site/home-assets");
+    eleventyConfig.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit)); //https://github.com/11ty/eleventy/issues/1368
     return {
         dir: {
             input: "_site",
